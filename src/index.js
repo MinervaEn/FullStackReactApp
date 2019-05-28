f06c1626e4fd6594bc0ea4aee45df5f055309f95
Minerva Enriquez (InConsulting Inc.) <v-maenr@microsoft.com>
2019-05-27 22:19:08 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+<div>
+    <h1>
+        Mr. Gato - initial draft.
+    </h1>
+</div>,
+document.getElementById('root')
+
+    )
